78a5dd90bc444298d09c20c31eed313a0682e704
Osawerengeka <mr.georgegevork@gmail.com>
2021-01-03 22:07:13 +0300

    6 lab

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 function contacts()
 {
     var dialog = document.querySelector('dialog.contacts');
-   //  var dialog = document.createElement("dialog");
-   // dialog.innerHTML("");
     if(dialog.hasAttribute('hidden'))
     {
         dialog.removeAttribute('hidden');
@@ -13,10 +11,29 @@ function contacts()
 function contactsClose()
 {
     var dialog = document.querySelector('dialog.contacts');
-    //  var dialog = document.createElement("dialog");
-    // dialog.innerHTML("");
     if(!dialog.hasAttribute('hidden'))
     {
         dialog.setAttribute('hidden','true');
+    }
+}
+function imageChange()
+{
+    let arr = ["img/1.jpg","img/2.png","img/3.png"];
+    var img = document.querySelector('img.images');
+    var i = 0;
+    while ( i < 3) {
+        if (arr[i] === img.getAttribute("src"))
+        {
+            if( i === 2)
+            {
+                img.setAttribute('src',arr[0]);
+                i = 3;
+            }
+            else {
+                img.setAttribute('src', arr[i + 1]);
+                i = 3;
+            }
+        }
+        i++
     }
 }
